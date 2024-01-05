d9cf4e32a43ecc7552063ebd17928403552b0194
Anurag Singh Naruka <anuragsinghnaruka@gmail.com>
2024-01-05 19:42:13 +0530

    routing error solved

--- src/App.jsx
+++ src/App.jsx
@@ -1,32 +1,19 @@
 // eslint-disable-next-line no-unused-vars
 import React from "react";
 import { Routes, Route } from "react-router-dom";
-import Home from "./pages/Home";
+import HomePage from "./pages/HomePage";
 import DnDFlow from "./pages/DnDFlow";
 import { BrowserRouter as Router } from "react-router-dom";
 
-
-
 const App = () => {
-  return (
-    <Router>
-      <div>
-        {/* Navigation for the home page */}
-        <nav>
-          <ul>
-            <li>
-              <Link to="/">Home</Link>
-            </li>
-          </ul>
-        </nav>
 
-        {/* Define routes for Home, FitbitHome, and NutrifitHome */}
+  return (
+      <Router>
         <Routes>
-          <Route path="/" element={<Home/>}/>
+          <Route path="/" element={<HomePage/>}/>
           <Route path="/protocolmap" element={<DnDFlow/>}/>
         </Routes>
-      </div>
-    </Router>
+      </Router>
   );
 };
 
