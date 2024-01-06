0bc3764114059cba107e70e2872e32ad7abf7ff2
Rahul Jain <rahulcom64@gmail.com>
2024-01-07 01:55:59 +0530

    Shared Workout UI added

--- src/pages/MyWorkouts.jsx
+++ src/pages/MyWorkouts.jsx
@@ -14,32 +14,57 @@ const MyWorkouts = () => {
     <>
       <section className="programs">
         <div className="container programstocontainer">
-          <div className="sectiontohead undefined">
-            <img className="me-4" src={dumbleImg} style={{ width: '50px' }} />
-            <h2 className="fw-bold">Your Workouts</h2>
-          </div>
-          <div className="programstowrapper">
-            <article className="card programstoprogram" style={{ width: '300px' }}>
-              <span className="w-100 h-100">
-                <AddCircleIcon fontSize="large"/>
-                <h2>Add Workout</h2>
-              </span>
-              {/* <h4 className="fw-bold">Workout-1</h4> */}
-              {/* <small className="text-secondary">This is the day that the lord has made. We will rejoice!</small> */}
-              {/* <Link className="bttn sm fw-bold mt-2" style={{ textDecoration: 'none' }}>Edit</Link> */}
-              {/* <Link className="bttn sm fw-bold" style={{ textDecoration: 'none' }} onClick={handleOpen}>Open modal</Link> */}
-            </article>
+          <div className="row">
+            <div className="col-8">
+              <div className="sectiontohead undefined">
+                <img className="me-4" src={dumbleImg} style={{ width: '50px' }} />
+                <h2 className="fw-bold">Your Workouts</h2>
+              </div>
+              <div className="programstowrapper">
+                <article className="card programstoprogram" style={{ width: '300px' }}>
+                  <span className="w-100 h-100">
+                    <AddCircleIcon fontSize="large" />
+                    <h2>Add Workout</h2>
+                  </span>
+                  {/* <h4 className="fw-bold">Workout-1</h4> */}
+                  {/* <small className="text-secondary">This is the day that the lord has made. We will rejoice!</small> */}
+                  {/* <Link className="bttn sm fw-bold mt-2" style={{ textDecoration: 'none' }}>Edit</Link> */}
+                  {/* <Link className="bttn sm fw-bold" style={{ textDecoration: 'none' }} onClick={handleOpen}>Open modal</Link> */}
+                </article>
 
-            <article className="card programstoprogram" style={{ width: '300px' }}>
-              <span className="w-100 h-100">
-                <img src={benchImg} className="w-100" />
-              </span>
-              <h4 className="fw-bold">Workout-1</h4>
-              {/* <small className="text-secondary">This is the day that the lord has made. We will rejoice!</small> */}
-              <Link className="bttn sm fw-bold mt-2" style={{ textDecoration: 'none' }}>Edit</Link>
-              {/* <Link className="bttn sm fw-bold" style={{ textDecoration: 'none' }} onClick={handleOpen}>Open modal</Link> */}
-            </article>
+                <article className="card programstoprogram" style={{ width: '300px' }}>
+                  <span className="w-100 h-100">
+                    <img src={benchImg} className="w-100" />
+                  </span>
+                  <h4 className="fw-bold">Workout-1</h4>
+                  {/* <small className="text-secondary">This is the day that the lord has made. We will rejoice!</small> */}
+                  <Link className="bttn sm fw-bold mt-2" style={{ textDecoration: 'none' }}>Edit</Link>
+                  {/* <Link className="bttn sm fw-bold" style={{ textDecoration: 'none' }} onClick={handleOpen}>Open modal</Link> */}
+                </article>
 
+              </div>
+            </div>
+
+
+            <div className="col-4">
+              <div className="sectiontohead undefined">
+                <img className="me-4" src={dumbleImg} style={{ width: '50px' }} />
+                <h2 className="fw-bold">Shared Workouts</h2>
+              </div>
+              <div className="programstowrapper">
+
+                <article className="card programstoprogram" style={{ width: '300px' }}>
+                  <span className="w-100 h-100">
+                    <img src={benchImg} className="w-100" />
+                  </span>
+                  <h4 className="fw-bold">Workout-1</h4>
+                  {/* <small className="text-secondary">This is the day that the lord has made. We will rejoice!</small> */}
+                  <Link className="bttn sm fw-bold mt-2" style={{ textDecoration: 'none' }}>Edit</Link>
+                  {/* <Link className="bttn sm fw-bold" style={{ textDecoration: 'none' }} onClick={handleOpen}>Open modal</Link> */}
+                </article>
+
+              </div>
+            </div>
           </div>
         </div>
       </section>
