9c0bc162c0b9c7cff51858e537b529a9a31d3bec
Rahul Jain <rahulcom64@gmail.com>
2024-01-07 04:41:55 +0530

    createWorkout UI updated

--- src/pages/CreateWorkout.jsx
+++ src/pages/CreateWorkout.jsx
@@ -9,6 +9,9 @@ import TextField from '@mui/material/TextField';
 import Input from '@mui/material/Input';
 import Button from '@mui/material/Button'
 import InputAdornment from '@mui/material/InputAdornment';
+import AddCircleIcon from '@mui/icons-material/AddCircle';
+import ShareIcon from '@mui/icons-material/Share';
+import benchImg from "../assets/images/bench.png"
 
 const CreateWorkout = () => {
 
@@ -16,55 +19,87 @@ const CreateWorkout = () => {
     return (
         <>
             <section className="programs">
-                <div className="container programstocontainer">
+                <div className="mx-auto programstocontainer" style={{width:'90%'}}>
                     <div className="sectiontohead undefined">
                         <img className="me-4" src={dumbleImg} style={{ width: '50px' }} />
-                        {/* <h2 className="fw-bold">Recommended Workout</h2> */}
-                        {/* <TextField id="standard-basic" label="Standard" variant="standard" className="bg-light text-dark"/> */}
                         <div class="form-floating mb-3">
                             <input type="email" class="form-control" id="floatingInput" placeholder="name@example.com" />
                             <label for="floatingInput">Workout Name</label>
                         </div>
                     </div>
-                    <div className="programstowrapper">
-                        <article className="card programstoprogram pb-0 pt-0 px-0 border" style={{ width: '300px' }}>
-                            <span className="w-100 h-100" style={{ borderRadius: '0 3rem 0 0' }}>
-                                <p className="p-0 m-0">Chest</p>
-                                <img src={chestPressImg} className="w-100" />
+
+                    <div className="programstowrapper d-flex flex-wrap">
+
+                        <article className="card programstoprogram d-flex flex-column align-items-center p-0" style={{ borderRadius: '25px', width:'250px' }} >
+                            <span className="h-100 m-0" style={{ width: '100%', borderRadius: '25px 25px 0 0' }}>
+                                <img src={benchImg} className="w-100" />
                             </span>
-                            <h4 className="fw-bold">Chest Press</h4>
-                            <div className="d-flex text-muted mt-3 px-2">
-                                {/* <span style={{ width: 'fit-content', fontSize: 'small' }}>765 kcal</span> */}
-
-                                <span className="mx-1" style={{ width: 'fit-content', fontSize: 'small' }}>
-                                    <Input
-                                        id="standard-adornment-weight"
-                                        endAdornment=
-                                        {<InputAdornment position="end">
-                                            kcal
-                                        </InputAdornment>}
-                                        aria-describedby="standard-weight-helper-text"
-                                        size="small"
-                                        className="bg-transparent ps-2 fw-bold"
-                                    />
-                                </span>
-
-                                <span className="ms-1" style={{ width: 'fit-content' }}>
-                                    <Input
-                                        id="standard-adornment-reps"
-                                        endAdornment=
-                                        {<InputAdornment position="end">
-                                            reps
-                                        </InputAdornment>}
-                                        aria-describedby="standard-weight-helper-text"
-                                        size="small"
-                                        className="bg-transparent ps-2 fw-bold"
-                                    />
-                                </span>
-                                {/* <span style={{ width: 'fit-content', fontSize: 'small' }}>15 reps</span> */}
+                            <div className="w-100 d-flex">
+                                <div className="w-50">
+                                    <p className="fw-bold text-white m-0 p-0">Chest Press</p>
+                                    <p className="text-secondary small m-0 p-0">Chest</p>
+                                </div>
+                                <div className="w-50">
+                                    <span className="p-0 m-0 w-100 border border-2" style={{fontSize: 'xx-small', borderRadius: '0px' }}>
+                                        765 kcal<br/>
+                                        15 reps
+                                    </span>
+                                </div>
                             </div>
-                            <div className="w-100 mb-0 py-2 bg-dark border border-2 border-white" style={{borderRadius:'0 0 0 3rem'}}>
-                                <Link variant="contained" className='text-light fw-bold' style={{textDecoration:'none'}}>ADD</Link>
+                        </article>
+                        
+
+                        <article className="card programstoprogram d-flex flex-column align-items-center p-0" style={{ borderRadius: '25px', width:'250px' }} >
+                            <span className="h-100 m-0" style={{ width: '100%', borderRadius: '25px 25px 0 0' }}>
+                                <img src={benchImg} className="w-100" />
+                            </span>
+                            <div className="w-100 d-flex">
+                                <div className="w-50">
+                                    <p className="fw-bold text-white m-0 p-0">Chest Press</p>
+                                    <p className="text-secondary small m-0 p-0">Chest</p>
+                                </div>
+                                <div className="w-50">
+                                    <span className="p-0 m-0 w-100 border border-2" style={{fontSize: 'xx-small', borderRadius: '0px' }}>
+                                        765 kcal<br/>
+                                        15 reps
+                                    </span>
+                                </div>
+                            </div>
+                        </article>
+
+                        <article className="card programstoprogram d-flex flex-column align-items-center p-0" style={{ borderRadius: '25px', width:'250px' }} >
+                            <span className="h-100 m-0" style={{ width: '100%', borderRadius: '25px 25px 0 0' }}>
+                                <img src={benchImg} className="w-100" />
+                            </span>
+                            <div className="w-100 d-flex">
+                                <div className="w-50">
+                                    <p className="fw-bold text-white m-0 p-0">Chest Press</p>
+                                    <p className="text-secondary small m-0 p-0">Chest</p>
+                                </div>
+                                <div className="w-50">
+                                    <span className="p-0 m-0 w-100 border border-2" style={{fontSize: 'xx-small', borderRadius: '0px' }}>
+                                        765 kcal<br/>
+                                        15 reps
+                                    </span>
+                                </div>
+                            </div>
+                        </article>
+
+                        <article className="card programstoprogram d-flex flex-column align-items-center p-0" style={{ borderRadius: '25px', width:'250px' }} >
+                            <span className="h-100 m-0" style={{ width: '100%', borderRadius: '25px 25px 0 0' }}>
+                                <img src={benchImg} className="w-100" />
+                            </span>
+                            <div className="w-100 d-flex">
+                                <div className="w-50">
+                                    <p className="fw-bold text-white m-0 p-0">Chest Press</p>
+                                    <p className="text-secondary small m-0 p-0">Chest</p>
+                                </div>
+                                <div className="w-50">
+                                    <span className="p-0 m-0 w-100 border border-2" style={{fontSize: 'xx-small', borderRadius: '0px' }}>
+                                        765 kcal<br/>
+                                        15 reps
+                                    </span>
+                                </div>
                             </div>
                         </article>
 
